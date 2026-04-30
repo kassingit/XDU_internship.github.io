bbfe7e62581699abbad366bcb7a6c9c487a60ecd
kassingit <kassingit@gmail.com>
2026-04-30 20:28:11 +0200

    OpenCV

--- OpenCV_Files/Pictures/read.py
+++ OpenCV_Files/Pictures/read.py
@@ -2,9 +2,25 @@ import cv2 as cv
 ## Image reading 
 #Since Read.py and the picture are in the same folder, we will just us the name of the file
 img = cv.imread('Intel-CPU1.jpg');
+
+"""
 cv.imshow('Image 0',img);
 cv.waitKey(0)
 cv.destroyAllWindows();
+"""
 
+# Image resizing 
 
-## video reading 
+def rescale_image( frame, scale=0.75):
+    height = int(frame.shape[0]*scale) # we put int() to transform frame.shape[0]*scale into an int
+    width = int(frame.shape[1]*scale)
+
+    dimension = (width,height) # /!\ NOT (heigth,width) because cv.resize() expect (width,height)
+
+    return cv.resize(frame, dimension, interpolation = cv.INTER_AREA)
+
+img_resized = rescale_image(img,0.5)
+cv.imshow('Image 0',img);
+cv.imshow("Image resized", img_resized)
+cv.waitKey(0)
+cv.destroyAllWindows();
--- OpenCV_Files/Videos/draft.py
+++ OpenCV_Files/Videos/draft.py
@@ -1,24 +1,9 @@
 import cv2 as cv
 
 vd = cv.VideoCapture('dog.mp4');
+vd2 =cv.VideoCapture('kitten.mp4');
 
-### -------------------------------- video reading
-"""
-while True:
-    isTrue, frame = vd.read()
-    if isTrue:
-        cv.imshow("video 1",frame);
-        #display the frames avery 20 ms and get out of the video when the letter 'd' is pressed
-        if cv.waitKey(20) & 0xFF ==ord('f') :
-            break;
-    else:
-        break;
 
-vc.release()
-cv.destroyedAllWindows()    
-"""
-
-##-----------------------------------  rescaling and resizing ( for image, video and live video)
 def rescale_frame( frame, scale=0.75):
     height = int(frame.shape[0]*scale) # we put int() to transform frame.shape[0]*scale into an int
     width = int(frame.shape[1]*scale)
@@ -28,24 +13,31 @@ def rescale_frame( frame, scale=0.75):
     return cv.resize(frame, dimension, interpolation = cv.INTER_AREA)
 
 
+
 while True:
     isTrue, frame = vd.read()
     frame_resized = rescale_frame(frame,0.5)
     if isTrue:
-        cv.imshow("Original video ",frame)
         cv.imshow("resized video ",frame_resized)
-
         #display the frames avery 20 ms and get out of the video when the letter 'd' is pressed
         if cv.waitKey(20) & 0xFF ==ord('f') :
             break;
     else:
         break;
 
+    isTrue, frame2 = vd2.read()
+    frame_resized = rescale_frame(frame2,0.5)
+    if isTrue:
+        cv.imshow("resized video2 ",frame_resized)
+        #display the frames avery 20 ms and get out of the video when the letter 'd' is pressed
+        if cv.waitKey(20) & 0xFF ==ord('f') :
+            break;
+    else:
+        break;
+
+
+    
+    
+
 vc.release()
 cv.destroyedAllWindows()  
-
-##----------------------------------- rescaling and resizing  ( for only live video)
-def changeRes(width,height):
-    Capture.set(3,width);
-    Capture.set(4,height);
- 
